47e4d80dc88bdd8cdc730013ed4f521c077bdf6a
Антон <anton@workout.su>
2024-03-20 10:47:49 +0300

    Initial Commit

---
+++ TestovoeZadanie1/TestovoeZadanie1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestovoeZadanie1App.swift
+//  TestovoeZadanie1
+//
+//  Created by Natalia D on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestovoeZadanie1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
